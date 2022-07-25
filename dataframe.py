a6aa604ee80dd6f9d52878e4092a4a61d5dfbeaa
Zach Gillis <zachgillis@me.com>
2022-07-25 14:12:00 -0400

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -2,14 +2,18 @@ import os
 import xarray as xr
 import numpy as np
 import pandas as pd
+import dask
 
 directory = '/lustre/cv/projects/casa/jsteeb/uc4/bda_data'
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     vis_xds = xr.open_zarr(f)
-    print(vis_xds)
-    #print(len(vis_xds.DATA))
-    #print(np.abs(vis_xds.DATA))
+    row = []
     fn = filename.split('_')
-    print(fn)
+    row.append(float(fn[7]))
+    row.append(float(fn[9].split('.vis.zarr')[0]))
+    row.append(len(vis_xds.DATA))
+    row.append(dask.compute(np.mean(np.abs(vis_xds.DATA))))
+    print(row)
+    break
