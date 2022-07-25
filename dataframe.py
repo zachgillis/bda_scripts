3750c28ed494ff58bdb6c6a0daf027023255e427
Zach Gillis <zachgillis@me.com>
2022-07-25 12:39:36 -0400

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -4,5 +4,4 @@ directory = '/lustre/cv/projects/casa/jsteeb/uc4/bda_data'
  
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
-    if os.path.isfile(f):
-        print(f)
+    print(f)
