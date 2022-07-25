97e005fb19fde0f23923fff8aa74b0242906778a
Zach Gillis <zachgillis@me.com>
2022-07-25 14:00:36 -0400

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,9 +1,15 @@
 import os
 import xarray as xr
+import numpy as np
+import pandas as pd
 
 directory = '/lustre/cv/projects/casa/jsteeb/uc4/bda_data'
- 
+
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     vis_xds = xr.open_zarr(f)
-    print(len(vis_xds.DATA))
+    print(vis_xds)
+    #print(len(vis_xds.DATA))
+    #print(np.abs(vis_xds.DATA))
+    fn = filename.split('_')
+    print(fn)
