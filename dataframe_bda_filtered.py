b3967743c14aa76eba96191b157bd156ee047da4
Zach Gillis <zachgillis@me.com>
2022-08-08 11:56:13 -0500

    update

---
+++ dataframe_bda_filtered.py
@@ -0,0 +1,28 @@
+import os
+import xarray as xr
+import numpy as np
+import pandas as pd
+import dask
+
+directory = '/lustre/cv/projects/casa/jsteeb/uc4/bda_data'
+
+data = []
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    vis_xds = xr.open_zarr(f)
+    print(vis_xds)
+    row = []
+    fn = filename.split('_')
+    row.append(float(fn[7]))
+    row.append(float(fn[9].split('.vis.zarr')[0]))
+    row.append(len(vis_xds.DATA))
+    row.append(dask.compute(((np.abs(vis_xds.DATA)).weighted(vis_xds.WEIGHT)).mean(skipna=True))[0].item(0))
+    data.append(row)
+    print(row)
+
+bda_df = pd.DataFrame(np.array(data), columns=['Decorrelation Factor', 'Max. Samples Averaged', 'Number of Visibilities', 'Average Amplitude of Visibilities'])
+print(bda_df)
+
+bda_df.to_csv('/users/zgillis/bda_scripts/bda.csv')
+bda_df.to_html('/users/zgillis/bda_scripts/bda.html')
