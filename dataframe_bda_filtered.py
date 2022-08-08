265722cc2fcaed3ae687040310f7f612d94de3cc
Zach Gillis <zachgillis@me.com>
2022-08-08 12:43:24 -0500

    Update dataframe_bda_filtered.py

--- dataframe_bda_filtered.py
+++ dataframe_bda_filtered.py
@@ -11,7 +11,7 @@ data = []
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     vis_xds = xr.open_zarr(f)
-    print(vis_xds.UVW.where(np.sqrt(np.sum((vis_xds.UVW) ** 2)) < 13590.4688491672))
+    print(vis_xds.UVW.where(np.sqrt(np.sum((vis_xds.UVW) ** 2)) < 13590.4688491672).compute())
     print(1)
     print(vis_xds)
     vis_xds_filtered = vis_xds.where(np.sqrt(np.sum((vis_xds.UVW) ** 2)) < 13590.4688491672)
